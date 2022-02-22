cda7474551011b6543a109a23c7dcd73a3a35cff
Ondrej Valenta <ondrej.valenta@keenmate.com>
2022-02-22 20:17:50 +0100

    First commit

---
+++ Prestige01/Prestige01/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Hello, World!");
+
+var x = (int y) => y * 2;
+
+Console.ReadLine();
